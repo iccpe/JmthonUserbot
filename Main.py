ff2e3813affb5d5467b878e81984b58364cd3e51
iWhiskey <121051784+iccpe@users.noreply.github.com>
2025-07-27 14:58:29 +0300

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,14 @@
+from telethon import TelegramClient, events
+import config
+import commands
+
+if not all([config.API_ID, config.API_HASH, config.SESSION]):
+    print("❌ تأكد من إضافة API_ID, API_HASH, SESSION في Render")
+    exit()
+
+client = TelegramClient(StringSession(config.SESSION), config.API_ID, config.API_HASH)
+
+commands.register_all_commands(client)
+
+print("✅ البوت بدأ العمل ...")
+client.run_until_disconnected()
